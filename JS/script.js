8fbad14d440d9c44c67429fbb59044f2fddcb987
Cody Snell <codyjsnell@yahoo.com>
2020-10-26 14:26:48 -0500

    initial files

---
+++ JS/script.js
@@ -0,0 +1,58 @@
+
+
+// Constants and State Variables (Data)
+
+// Constant Data
+const base_url='https://cors-anywhere.herokuapp.com/https://www.wikia.com/api/v1/Wikis/ByString?expand=1&string='
+
+let wiki;
+
+
+// cached elements
+
+const $wikis = $('#wikis')
+// Attached Event Listeners
+
+$('form').on('submit', getData)
+
+
+
+
+function getData(evt){
+    evt.preventDefault()
+
+    const search = $('#search').val()
+    
+    const data = $.ajax({url:`https://cors-anywhere.herokuapp.com/https://www.wikia.com/api/v1/Wikis/ByString?expand=1&string=${search}&lang=en%2C+es&limit=25&batch=1&includeDomain=true/`,
+
+    })
+        .then(
+            (data)=>{
+                console.log(data)
+                wiki=data
+                render()
+                
+        },
+        (error)=> {
+            console.log("bad request: ", error)
+          }
+    )
+}
+
+function render(){
+    const dataArray = wiki.items.map(wiki=> {
+        return`
+        <article class="wiki">
+        <a href="${wiki.url}" target="_blank"><img class="logo" src="${wiki.image}"><img class="logo" src="${wiki.wordmark}">
+            <h3 id="name">${wiki.name}</h3>
+        </a>
+
+        <p id="lang"><strong>Language: </strong>${wiki.lang}</p>
+        <p id="users"><strong>Users: </strong>${wiki.stats.users}</p>
+        <p id="details"><strong>Details: </strong>${wiki.desc}</p>
+    </article>
+        `
+    })
+    $wikis.html(dataArray)
+
+}
