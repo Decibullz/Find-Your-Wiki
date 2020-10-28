5e6df6d0e78ceaa40142ae3b12cbf32a686ed88e
Cody Snell <codyjsnell@yahoo.com>
2020-10-28 09:42:36 -0500

    adds dark-mode toggle

--- JS/script.js
+++ JS/script.js
@@ -66,3 +66,21 @@ function render(){
 	});
 
 }
+
+// Select the button
+const btn = document.querySelector(".btn-toggle");
+// Select the stylesheet <link>
+const theme = document.querySelector("#theme-link");
+
+// Listen for a click on the button
+btn.addEventListener("click", function() {
+  // If the current URL contains "ligh-theme.css"
+  if (theme.getAttribute("href") == "./CSS/light-theme.css") {
+    // ... then switch it to "dark-theme.css"
+    theme.href = "./CSS/style.css";
+  // Otherwise...
+  } else {
+    // ... switch it to "light-theme.css"
+    theme.href = "./CSS/light-theme.css";
+  }
+});
